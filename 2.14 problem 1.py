6156620ac911101224ca00ebcbf67780953f6302
ynvj <jvnycb@gmail.com>
2026-06-07 23:24:24 -0600

    Exercise 2.14 Problem 1 - Jovanny (CSCI110-001)

---
+++ 2.14 problem 1.py
@@ -0,0 +1,12 @@
+one = "All "
+two = "work "
+three = "and "
+four = "no "
+five = "play "
+six = "makes "
+seven = "Jack "
+eight = "a "
+nine = "dull "
+ten = "boy"
+eleven = "."
+print( one + two + three + four + five + six + seven + eight + nine + ten + eleven)
