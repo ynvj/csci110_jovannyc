edcf7a15f4a5ceb451bdf25972fefe0fe32fc123
ynvj <jvnycb@gmail.com>
2026-06-08 00:49:38 -0600

    Chapter 3 problem 11 - Jovanny(CSCI110-001)

---
+++ Section 3.8 Number 11.py
@@ -0,0 +1,25 @@
+# ----------------------------------------
+# Program:   Chapter 3, 3.8 problem 11
+# Author:    ynvj
+# Name:      Jovanny Cano
+# Date:      06/08/2026
+# ----------------------------------------
+
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("lightblue")
+star = turtle.Turtle()
+star.color("blue")
+
+star.left(72)
+star.forward(150)
+star.right(144)
+star.forward(150)
+star.left(216)
+star.forward(150)
+star.right(144)
+star.forward(150)
+star.left(216)
+star.forward(150)
+
+wn.mainloop()
