19230a3db1ad12e251ef7cbbb63b2db73c518f9a
ynvj <jvnycb@gmail.com>
2026-06-13 00:54:29 -0600

    Exercise 5.14 Problem 1 - Jovanny (CSCI110-001)

---
+++ Section 5.14 Number 1.py
@@ -0,0 +1,24 @@
+# ----------------------------------------
+# Program:   Chapter 5 Exercise 1 
+# Author:    ynvj
+# Name:      Jovanny Cano
+# Date:      06/13/2026
+# ----------------------------------------
+def days_of_week (day_number):
+    if day_number == 0:
+        return("Sunday")
+    elif day_number ==1:
+        return("Monday")
+    elif day_number ==2:
+        return("Tuesday")
+    elif day_number ==3:
+        return("Wednesday")
+    elif day_number ==4:
+        return("Thursday")
+    elif day_number ==5:
+        return("Friday")
+    elif day_number ==6:
+        return("Saturday")
+
+    
+
